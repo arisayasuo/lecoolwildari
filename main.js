b112acac9f80bb95385a13549d94ba3a09361b77
arisayasuo <arisa.yasuo@gmail.com>
2022-08-19 10:44:35 +0200

    feedback

--- main.js
+++ main.js
@@ -5,15 +5,29 @@
 const nav = document.getElementById('nav');
 const hamburger = document.getElementById('hamburger');
 
+//Make Hamburger with 3 elements no pseudo element
+//
+const hamburgerBefore = window.getComputedStyle(
+	document.querySelector('#hamburger'), ':before'
+)
+const hamburgerAfter = window.getComputedStyle(
+	document.querySelector('#hamburger'), ':after'
+)
+
+console.log(hamburgerBefore);
+console.log(hamburgerAfter);
+
+const minWidth = 480
+
 function resizeWindow(event){
     const width = window.innerWidth;
-    //console.log(width);
+    console.log(width);
 
-    if(width < 480){
+    if(width <= minWidth){
         nav.style.display = "none";
         hamburger.style.display = "block";
-    } else{
-        nav.style.display = "block";
+    }else{
+        nav.style.display = "flex";
         hamburger.style.display = "none";
     }
 }
@@ -26,7 +40,19 @@ window.addEventListener('resize', resizeWindow);
 hamburger.addEventListener('click',showMobileNav);
 
 function showMobileNav(event){
-    console.log("mobile ver nav is under construction");
+    nav.style.display = "block";
+    nav.classList.add('.navClicked');
+    hamburger.style.display = "none";
+    // hamburgerBefore.classList.add('.closeBefore');
+    // hamburgerAfter.classList.add('.closeAfter');
 }
 
 
+
+
+// Header
+//     nav
+//         logo
+//         hamburger
+//         menu 
+        
