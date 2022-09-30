2224ef9d5481158647da75cee0c3a6909f73c269
arisayasuo <arisa.yasuo@gmail.com>
2022-09-30 16:51:36 +0900

    updated hamburger

--- main.js
+++ main.js
@@ -3,19 +3,28 @@
 //Switch Nav/Hamburger --- 
 ///////////////////////////////////////////////////
 const nav = document.getElementById('nav');
+const mainnav = document.getElementById('mainnav');
 const hamburger = document.getElementById('hamburger');
 
-//Make Hamburger with 3 elements no pseudo element
-//
-const hamburgerBefore = window.getComputedStyle(
-	document.querySelector('#hamburger'), ':before'
-)
-const hamburgerAfter = window.getComputedStyle(
-	document.querySelector('#hamburger'), ':after'
-)
+// //Make Hamburger with 3 elements no pseudo element
+// //
+// const hamburgerBefore = window.getComputedStyle(
+// 	document.querySelector('#hamburger'), ':before'
+// )
+// const hamburgerAfter = window.getComputedStyle(
+// 	document.querySelector('#hamburger'), ':after'
+// )
 
-console.log(hamburgerBefore);
-console.log(hamburgerAfter);
+const line1 = document.getElementById('line1');
+const line2 = document.getElementById('line2');
+const line3 = document.getElementById('line3');
+
+
+console.log(line1);
+console.log(line2);
+console.log(line3);
+console.log(mainnav);
+
 
 const minWidth = 480
 
@@ -26,9 +35,11 @@ function resizeWindow(event){
     if(width <= minWidth){
         nav.style.display = "none";
         hamburger.style.display = "block";
+
     }else{
         nav.style.display = "flex";
         hamburger.style.display = "none";
+
     }
 }
 
@@ -40,12 +51,18 @@ window.addEventListener('resize', resizeWindow);
 hamburger.addEventListener('click',showMobileNav);
 
 function showMobileNav(event){
-    nav.style.display = "block";
-    nav.classList.add('.navClicked');
-    hamburger.style.display = "none";
+    //nav.style.display = "block";
+    nav.classList.toggle('.navClicked');
+    //hamburger.style.display = "none";
+    mainnav.classList.toggle('.mainnavClicked');
+    line1.classList.toggle('rotateA');
+    line2.classList.toggle('rotateB');
+    line3.classList.toggle('rotateC');
     // hamburgerBefore.classList.add('.closeBefore');
     // hamburgerAfter.classList.add('.closeAfter');
 }
+
+
 
 
 
@@ -55,4 +72,10 @@ function showMobileNav(event){
 //         logo
 //         hamburger
 //         menu 
+
+//my problems 20220930
+// main-nav position (after I changed the hierarchy it got messed up)
+// hide hamburger menu in the beginning (display none is not working)
+// hamburger menu's line2 doesn't rotate
+// have to make main-nav to stack vertical when it's clicked
         
